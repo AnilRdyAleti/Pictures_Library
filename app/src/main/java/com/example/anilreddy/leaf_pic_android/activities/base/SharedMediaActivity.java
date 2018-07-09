4688c850f57c1fbe2983848dd28790e98ae3e8ab
AnilRdyAleti <AnilRdyAleti@github.com>
2018-07-09 16:07:30 -0400

    Added String file to project

--- app/src/main/java/com/example/anilreddy/leaf_pic_android/activities/base/SharedMediaActivity.java
+++ app/src/main/java/com/example/anilreddy/leaf_pic_android/activities/base/SharedMediaActivity.java
@@ -1,4 +1,10 @@
 package com.example.anilreddy.leaf_pic_android.activities.base;
 
-public class SharedMediaActivity {
+public abstract class SharedMediaActivity extends BaseActivity {
+
+    private int REQUEST_CODE_SD_CARD_PERMISSIONS = 42;
+
+    public void requestSdCardPermissions() {
+      //  AlertDialog textDialog = AlertDialogsHelper.getTextDialog()
+    }
 }

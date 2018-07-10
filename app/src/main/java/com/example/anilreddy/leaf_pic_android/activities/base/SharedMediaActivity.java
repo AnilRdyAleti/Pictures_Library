a5e8e4d5462fd55db91eb46aedbb2724d01fa968
AnilRdyAleti <AnilRdyAleti@github.com>
2018-07-09 21:34:08 -0400

    created alert dialog insert text and normal text in alert dialog helper class

--- app/src/main/java/com/example/anilreddy/leaf_pic_android/activities/base/SharedMediaActivity.java
+++ app/src/main/java/com/example/anilreddy/leaf_pic_android/activities/base/SharedMediaActivity.java
@@ -1,10 +1,15 @@
 package com.example.anilreddy.leaf_pic_android.activities.base;
 
+import android.support.v7.app.AlertDialog;
+
+import com.example.anilreddy.leaf_pic_android.R;
+import com.example.anilreddy.leaf_pic_android.utils.AlertDialogsHelper;
+
 public abstract class SharedMediaActivity extends BaseActivity {
 
     private int REQUEST_CODE_SD_CARD_PERMISSIONS = 42;
 
     public void requestSdCardPermissions() {
-      //  AlertDialog textDialog = AlertDialogsHelper.getTextDialog()
+        AlertDialog textDialog = AlertDialogsHelper.getTextDialog(this, R.string.sd_card_write_permission_title, R.string.sd_card_permissions_message);
     }
 }
